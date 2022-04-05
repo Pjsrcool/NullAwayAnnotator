3d0ddfc56ccd5c5e0ce31e59bde08c1f88021298
nimakarimipour <karimipour.nima@gmail.com>
2022-04-05 16:26:42 -0700

    update

---
+++ core/src/main/java/edu/ucr/cs/riple/core/metadata/trackers/ParameterRegionTracker.java
@@ -0,0 +1,32 @@
+package edu.ucr.cs.riple.core.metadata.trackers;
+
+import edu.ucr.cs.riple.core.FixType;
+import edu.ucr.cs.riple.core.metadata.AbstractRelation;
+import edu.ucr.cs.riple.injector.Fix;
+
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Set;
+
+public class ParameterRegionTracker extends AbstractRelation<TrackerNode> implements RegionTracker{
+
+    private final FixType fixType;
+
+    public ParameterRegionTracker(Path path) {
+        super(path);
+        this.fixType = FixType.PARAMETER;
+    }
+
+    @Override
+    protected TrackerNode addNodeByLine(String[] values) {
+        return null;
+    }
+
+    @Override
+    public Set<Region> getRegions(Fix fix) {
+        if (!fix.location.equals(fixType.name)) {
+            return null;
+        }
+        return Collections.singleton(new Region(fix.method, fix.className));
+    }
+}
